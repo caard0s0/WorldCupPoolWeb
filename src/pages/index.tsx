3046eb34610bc106601f573646fabbb4a8cb3b4b
Vinicius <vinicardoso216@gmail.com>
2022-11-02 17:21:08 -0300

    feat: API connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,16 @@ import appPreviewImg from '../assets/app-nlw-copa-preview.png';
 import logoImg from '../assets/logo.svg';
 import usersAvatarExampleImg from '../assets/users-avatar-example.png';
 import iconCheckImg from '../assets/icon-check.svg';
+import { api } from '../lib/axios';
+
+interface HomeProps {
+  poolCount: number;
+  guessCount: number;
+  userCount: number;
+}
 
 
-export default function Home() {
+export default function Home(props: HomeProps) {
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
 
@@ -24,7 +31,7 @@ export default function Home() {
           <strong className="text-gray-100 text-xl flex items-center gap-2">
 
               <span className="text-ignite-500">
-                +12.592
+                +{props.userCount}
               </span>
 
               people are already using
@@ -62,7 +69,7 @@ export default function Home() {
             <Image src={iconCheckImg} alt="" />
 
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+2.834</span>
+              <span className="font-bold text-2xl">+{props.poolCount}</span>
               <span>Pools created</span>
             </div>
 
@@ -77,7 +84,7 @@ export default function Home() {
             <Image src={iconCheckImg} alt="" />
             
             <div className="flex flex-col">
-              <span className="font-bold text-2xl">+192.847</span>
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
               <span>Guesses sent</span>
             </div>
 
@@ -98,13 +105,22 @@ export default function Home() {
 }
 
 
-// export const getServerSideProps = async () => {
-//   const response = await fetch('http://localhost:3333/pools/count')
-//   const data = await response.json()
+export const getServerSideProps = async () => {
+  const [
+    poolCountResponse, 
+    guessCountResponse,
+    userCountResponse] 
+    = await Promise.all([
+    api.get('pools/count'),
+    api.get('guesses/count'),
+    api.get('users/count')
+  ])
 
-//   return {
-//     props: {
-//       count: data.count,
-//     }
-//   }
-// }
+  return {
+    props: {
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count
+    }
+  }
+}
