bfb708b3c9f32ebf8eafac6c22a32707e5bcb5e2
Vinicius <vinicardoso216@gmail.com>
2022-11-02 17:39:01 -0300

    feat: Pool creation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { FormEvent, useState } from 'react';
+import { api } from '../lib/axios';
 import Image from 'next/image';
+
 import appPreviewImg from '../assets/app-nlw-copa-preview.png';
 import logoImg from '../assets/logo.svg';
 import usersAvatarExampleImg from '../assets/users-avatar-example.png';
 import iconCheckImg from '../assets/icon-check.svg';
-import { api } from '../lib/axios';
 
 interface HomeProps {
   poolCount: number;
@@ -13,6 +15,28 @@ interface HomeProps {
 
 
 export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('');
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      }); 
+
+      const { code } = response.data
+
+      await navigator.clipboard.writeText(code)
+
+      alert('Pool created successfully, the code has been copied to the clipboard!')
+    
+      setPoolTitle('');
+    } catch (err) {
+      alert('Failed to create pool, try again!')
+    }
+  }
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
 
@@ -40,13 +64,15 @@ export default function Home(props: HomeProps) {
 
         </div>
 
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
 
           <input 
-            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100"
             type="text" 
             required 
-            placeholder="What is the name of your pool?" 
+            placeholder="What is the name of your pool?"
+            onChange={event => setPoolTitle(event.target.value)} 
+            value={poolTitle}
           />
           
           <button 
